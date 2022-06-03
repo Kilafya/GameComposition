988a1fea8f684b0611d6fb71b03c0eb14719bdc8
drayl <sunrise.161@mail.ru>
2022-06-03 17:25:29 +0300

    added layout domain and data

--- app/src/main/java/com/kilafyan/gamecomposition/presentation/MainActivity.kt
+++ app/src/main/java/com/kilafyan/gamecomposition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kilafyan.gamecomposition
+package com.kilafyan.gamecomposition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kilafyan.gamecomposition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
